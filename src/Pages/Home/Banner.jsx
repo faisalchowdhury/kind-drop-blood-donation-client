5d7980dc19bb4be97428900cb3e992cfdafe16b0
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-07-09 00:14:53 +0600

    UI - Transition animation added to hero section using Motion

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
+import { motion } from "motion/react";
 const Banner = () => {
   return (
-    <div>
+    <section>
       <div className="relative bg-[url('https://i.ibb.co/Rk8xkKWd/pexels-kirill-dratsevich-237907001-12227661-1.jpg')] bg-cover h-[500px] rounded-2xl overflow-hidden">
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/40 rounded-2xl"></div>
 
         {/* Content */}
-        <div className="relative z-10 flex items-center h-full max-w-[50%] px-10">
+        <motion.div
+          initial={{ y: 300, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          opacity
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="relative z-10 flex items-center h-full max-w-[50%] px-10">
           <div className="space-y-3">
             <p className="text-white">
               Join us in building a healthier tomorrow by giving the gift of
@@ -27,9 +32,9 @@ const Banner = () => {
               </button>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </section>
   );
 };
 
