b7d6cb6b692ab31f92bf90be3ce34833905fa1ca
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-07-12 18:16:28 +0600

    All donation request features implemented

--- src/Dashboard/AllDonationRequests.jsx
+++ src/Dashboard/AllDonationRequests.jsx
@@ -1,47 +1,67 @@
 import React from "react";
+import useAxiosBase from "../Hooks/useAxiosBase";
+import { useQuery } from "@tanstack/react-query";
 
 export default function AllDonationRequests() {
+  const axiosBase = useAxiosBase();
+
+  const { data: requests = [] } = useQuery({
+    queryKey: ["requests"],
+    queryFn: () =>
+      axiosBase.get("/all-donation-requests").then((res) => res.data),
+  });
+
+  console.log(requests);
   return (
-    <div className="p-4 overflow-x-auto max-w-full">
-      <h2 className="text-xl font-semibold mb-4 text-primary">
-        All Donation Requests
-      </h2>
-      <div className="inline-block min-w-full align-middle">
-        <table className="table table-sm table-pin-rows table-pin-cols w-max max-w-full">
-          <thead>
-            <tr>
-              <th className="bg-white">No.</th>
-              <td>Recipient Name</td>
-              <td>Location</td>
-              <td>Date</td>
-              <td>Time</td>
-              <td>Blood Group</td>
-              <td>Status</td>
-              <td>Actions</td>
-            </tr>
-          </thead>
-          <tbody>
-            {[1, 2, 3].map((id) => (
-              <tr key={id} className="hover:bg-slate-200">
-                <th className="bg-white">{id}</th>
-                <td>John Doe</td>
-                <td>Dhaka, Dhanmondi</td>
-                <td>2025-07-15</td>
-                <td>10:30 AM</td>
-                <td>A+</td>
-                <td>In Progress</td>
-                <td className="flex gap-1 flex-wrap">
-                  <button className="btn btn-success btn-xs">Done</button>
-                  <button className="btn btn-error btn-xs">Cancel</button>
-                  <button className="btn btn-info btn-xs">Edit</button>
-                  <button className="btn btn-error btn-xs">Delete</button>
-                  <button className="btn btn-neutral btn-xs">View</button>
-                </td>
+    <>
+      <div className="bg-slate-50 p-4 shadow rounded">
+        <h2 className="text-xl font-semibold mb-4 text-primary">
+          All Donation Requests
+        </h2>
+        <div className="overflow-x-auto">
+          <table className="table table-sm table-pin-rows table-pin-cols">
+            <thead>
+              <tr>
+                <th className="bg-white">No.</th>
+                <td>Recipient Name</td>
+                <td>Location</td>
+                <td>Date</td>
+                <td>Time</td>
+                <td>Blood Group</td>
+                <td>Status</td>
+                <td>Actions</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {requests.map((request, id) => (
+                <tr key={request._id} className="hover:bg-slate-200">
+                  <th className="bg-white">{id + 1}</th>
+                  <td>{request.recipientName}</td>
+                  <td>
+                    District - {request.recipientDistrict} <br /> Upazila -{" "}
+                    {request.recipientUpazila} <br />
+                    Address Line - {request.addressLine}
+                  </td>
+                  <td>{request.donationDate}</td>
+                  <td>{request.donationTime}</td>
+
+                  <td>{request.bloodGroup}</td>
+                  <td>{request.status}</td>
+                  <td className="grid grid-cols-2 gap-1 items-center justify-center">
+                    <button className="btn btn-success btn-xs">Done</button>
+                    <button className="btn btn-error btn-xs">Cancel</button>
+                    <button className="btn btn-info btn-xs">Edit</button>
+                    <button className="btn btn-error btn-xs">Delete</button>
+                    <button className="btn btn-neutral btn-xs col-span-2">
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
